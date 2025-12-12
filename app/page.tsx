077ff5827831d48bdd754a1583f95b2ac8c616ef
Dylan Kruyff <dylan.kruyff@gmail.com>
2025-12-12 14:05:04 +0100

    chore: decrease page padding

--- app/page.tsx
+++ app/page.tsx
@@ -40,7 +40,7 @@ const sections: { title: string; icon: LucideIcon; description: string }[] = [
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center gap-16 py-32 px-16 bg-white dark:bg-black sm:items-start">
+      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center gap-16 py-16 px-16 bg-white dark:bg-black sm:items-start">
         <Header />
         {sections.map((section) => (
           <Section
