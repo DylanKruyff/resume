afc8d41acc4b0d602733d32588f6e5679b81fc6c
Dylan Kruyff <dylan.kruyff@gmail.com>
2025-12-12 13:40:56 +0100

    chore: make header mobile friendly

---
+++ app/header.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import { MapPinIcon, MailIcon, LanguagesIcon } from "lucide-react";
+import { ContactItem } from "../components/ui/contact-item";
+import Image from "next/image";
+import { useIsMobile } from "../hooks/use-mobile";
+import { cn } from "../lib/utils";
+
+export function Header() {
+  const isMobile = useIsMobile();
+
+  return (
+    <div className="flex flex-col-reverse md:flex-row items-center justify-between gap-4 w-full">
+      <div className="flex flex-col gap-4">
+        <h1 className="text-4xl font-bold">Dylan Kruyff</h1>
+        <p className="text-lg italic flex">DevOps Software Engineer</p>
+        <div className="flex flex-col gap-2">
+          <ContactItem icon={MapPinIcon}>Utrecht, The Netherlands</ContactItem>
+          <ContactItem icon={MailIcon}>
+            <a href="mailto:dylan.kruyff@gmail.com">dylan.kruyff@gmail.com</a>
+          </ContactItem>
+          <ContactItem icon={LanguagesIcon}>English, Dutch</ContactItem>
+        </div>
+      </div>
+      <div
+        className={cn(
+          "rounded-lg overflow-hidden",
+          isMobile && "w-32 h-32 rounded-full"
+        )}
+      >
+        <Image
+          src="/profile.jpg"
+          alt="Dylan Kruyff"
+          width={150}
+          height={150}
+          className="object-cover object-top"
+        />
+      </div>
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -3,15 +3,11 @@ import {
   BriefcaseIcon,
   CodeIcon,
   GraduationCapIcon,
-  LanguagesIcon,
   LucideIcon,
-  MailIcon,
-  MapPinIcon,
   UserIcon,
 } from "lucide-react";
 import { Section } from "../components/ui/section";
-import Image from "next/image";
-import { ContactItem } from "../components/ui/contact-item";
+import { Header } from "./header";
 
 const sections: { title: string; icon: LucideIcon; description: string }[] = [
   {
@@ -45,30 +41,7 @@ export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center gap-16 py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <div className="flex justify-between w-full">
-          <div className="flex flex-col gap-4">
-            <h1 className="text-4xl font-bold">Dylan Kruyff</h1>
-            <p className="text-lg italic flex">DevOps Software Engineer</p>
-            <div className="flex flex-col gap-2">
-              <ContactItem icon={MapPinIcon}>
-                Utrecht, The Netherlands
-              </ContactItem>
-              <ContactItem icon={MailIcon}>
-                <a href="mailto:dylan.kruyff@gmail.com">
-                  dylan.kruyff@gmail.com
-                </a>
-              </ContactItem>
-              <ContactItem icon={LanguagesIcon}>English, Dutch</ContactItem>
-            </div>
-          </div>
-          <Image
-            src="/profile.jpg"
-            alt="Dylan Kruyff"
-            width={150}
-            height={150}
-            className="rounded-lg"
-          />
-        </div>
+        <Header />
         {sections.map((section) => (
           <Section
             key={section.title}
